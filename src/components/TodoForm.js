ee15064de207ecec10824bbb46cfac8ac6d4036a
paul <syrovatskyi93@gmail.com>
2020-12-02 08:46:20 +0200

    TodoList Component was changed

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -11,10 +11,10 @@ function TodoForm(props) {
   const handleSubmit = e => {
     e.preventDefault();
 
-    /* props.onSubmit({
-      id: Math.floor(Math.random() * 1000),
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
       text: input
-    }) */
+    })
 
     setInput('')
   }
@@ -30,6 +30,7 @@ function TodoForm(props) {
         placeholder="Add a todo"
         value={input}
         name="text"
+        autoComplete="off"
         className="todo-input"
         onChange={handleChange}
       />
