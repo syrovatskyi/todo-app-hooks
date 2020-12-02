05e796ef1d99e24358f398d7d23b88d28c4bc75e
paul <syrovatskyi93@gmail.com>
2020-12-02 10:31:07 +0200

    added remove and edit functions

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
 
   const [input, setInput] = useState('')
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.focus()
+  })
 
   const handleChange = e => {
     setInput(e.target.value)
@@ -33,6 +39,7 @@ function TodoForm(props) {
         autoComplete="off"
         className="todo-input"
         onChange={handleChange}
+        ref={inputRef}
       />
 
       <button className="todo-button">
